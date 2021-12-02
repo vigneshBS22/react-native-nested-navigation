9e3953661aa39765da402b5496f7ba044d158be6
vigneshBS22 <vigneshb@geekyants.comm>
2021-12-02 17:47:35 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+const App = () => {
+  return <View></View>;
+};
+
+export default App;
---
+++ android/app/src/main/java/com/rn_navgation_practice/MainActivity.java
@@ -0,0 +1,20 @@
+package com.rn_navgation_practice;
+
+import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "RN_navgation_practice";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
+}
